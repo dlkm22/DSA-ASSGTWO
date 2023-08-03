babec12f83932e40ab45e6f4b6dc652d74c4637e
dlkm22 <115967278+dlkm22@users.noreply.github.com>
2023-08-03 10:28:08 +0800

    Queue update

--- DSA-ASSG2/DSA-ASSG2.cpp
+++ DSA-ASSG2/DSA-ASSG2.cpp
@@ -1,20 +1,39 @@
-// DSA-ASSG2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Queue.h"
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int option = 1;
+
+    while (option != 0) {
+        cout << "Select operation: ";
+        cin >> option;
+
+        if (option == 1) {
+            cout << "View Incoming Orders" << endl;
+        }
+
+        else if (option == 2) {
+            cout << "Update status of order" << endl;
+        }
+
+        else if (option == 3) {
+            cout << "View customer information for an order" << endl;
+        }
+
+        else if (option == 0) {
+            cout << "Goodbye" << endl;
+            break;
+        }
+
+        else {
+            cout << "Not a valid option! Please try again" << endl;
+            continue;
+        }
+
+
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
