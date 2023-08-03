cad1e3a2f72dbc6859c4eed70bc1c44944b89939
dlkm22 <115967278+dlkm22@users.noreply.github.com>
2023-08-03 11:04:25 +0800

    Menu update

--- DSA-ASSG2/DSA-ASSG2.cpp
+++ DSA-ASSG2/DSA-ASSG2.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include "Queue.h"
+#include "List.h"
 using namespace std;
 
+List populateFoodList(List foodList) {
+    foodList.add("WcChicken");
+    foodList.add("WcSpicy");
+    foodList.add("BigWac");
+    foodList.add("Woke Zero");
+    return foodList;
+}
 
 int main()
 {
     int option = 1;
+    Queue orderQueue; //for admin
+    Queue customerQueue; //for customer
+
+    List foodList;
+    foodList.add("WcChicken");
+    foodList.add("WcSpicy");
+    foodList.add("BigWac");
+    foodList.add("Woke Zero");
+    //foodList = populateFoodList(foodList);
+
 
     while (option != 0) {
         cout << "Select operation: ";
@@ -21,6 +39,13 @@ int main()
 
         else if (option == 3) {
             cout << "View customer information for an order" << endl;
+        }
+        
+        else if (option == 4) { //changes
+            cout << "Create New Order" << endl;
+            for (int i = 0; i < foodList.getLength(); i++) {
+                cout << foodList.get(i) << endl;
+            }
         }
 
         else if (option == 0) {
