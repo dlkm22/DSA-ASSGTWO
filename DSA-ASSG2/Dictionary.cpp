205663a1f5ff3f21de9e501421b7eebe624cc6d3
dlkm22 <115967278+dlkm22@users.noreply.github.com>
2023-08-10 10:31:08 +0800

    dictionary update

--- DSA-ASSG2/Dictionary.cpp
+++ DSA-ASSG2/Dictionary.cpp
@@ -9,64 +9,90 @@ Dictionary::Dictionary() {
 	}
 }
 
-Dictionary::Dictionary() { size = 0, lastNode; }
-
-Dictionary::~Dictionary() {}
-
-
-
-int Dictionary::hash(KeyType key)
-{
-	int keyValue = 0;
-	for (int i = 0; i <= key.length(); i++)
-	{
-		keyValue += charValue(key[i]);
+Dictionary::~Dictionary() {
+	for (int i = 0; i < MAX_SIZE; i++) {
+		Node* current = items[i];
+		while (current != nullptr) {
+			Node* temp = current;
+			current = current->next;
+			delete temp;
+		}
 	}
-	return keyValue;
 }
 
+int Dictionary::hash(KeyType key) {
+	int hash = 0;
+	int index;
+	for (int i = 0; i < key.length(); i++) {
+		char c = key[i];
+		if (isalpha(c))
+		{
+			if (isupper(c))
+				hash += (int)c - (int)'A';
 
-int Dictionary::charValue(char c)
-{
-	if (isalpha(c))
-	{
-		if (isupper(c))
-			return (int)c - (int)'A';
+
+			else
+				hash += (int)c - (int)'a' + 26;
+		}
 		else
-			return (int)c - (int)'a' + 26;
+			return -1;
 	}
-	else if (isdigit(c))
-	{
-		return (int)c; 
-	}
-	else
-		return -1;
-
+	index = hash % MAX_SIZE;
+	return index;
 }
 
 
 bool Dictionary::add(KeyType newKey, ItemType newItem) {
 	int index = hash(newKey);
 
-	if (size == 0)
-	{
-		items[0] = temp;
-		lastNode.next = temp;
-		size++;
-		return true;
+	if (items[index] == nullptr) {
+		Node* newNode = new Node;
+		newNode->key = newKey;
+		newNode->item = newItem;
+		newNode->next = nullptr;
+
+		items[index] = newNode;
 	}
-	else
-	{
-		for (int i = 0; i < MAX_SIZE; i++)
-		{
-			if (items[i]->key == newKey)
-			{
-				return false;
+	else {
+		Node* current = items[index]; //set current pointer to point to first node at index
+		if (current->key == newKey) {
+			return false; //key exists
+		}
+		while (current != nullptr) {
+			current = current->next;
+			if (current->key == newKey) {
+				return false; //key exists
+			}
+			Node* newNode = new Node;
+			newNode->key = newKey;
+			newNode->item = newItem;
+			newNode->next = nullptr;
+			current->next = newNode;
+		}
+	}
+	size++;
+	return true;
+}
+
+
+void Dictionary::remove(KeyType key) {
+	int index = hash(key);
+	Node* current = items[index];
+	Node* prev = nullptr;
+
+	while (current != nullptr) {
+		if (current->key == key) {
+			if (prev == nullptr) {
+				// If the node to remove is the first node in the chain
+				items[index] = current->next;
 			}
 			else {
 				// If the node to remove is in the middle or end of the chain
 				prev->next = current->next;
 			}
+			delete current;
+			size--;
+			return; // Node found and removed, exit the function
 		}
 		prev = current;
 		current = current->next;
@@ -83,16 +109,16 @@ ItemType Dictionary::get(KeyType key) {
 	}
 }
 
-void Dictionary::remove(KeyType key)
-{
-	key = hash(key);
-	for (int i = 0; i < size; i++)
-	{
-		if (items[i]->key == key)
-		{
-			items[i] = nullptr;
-		}
+bool Dictionary::isEmpty() {
+	if (size == 0) {
+		return true;
 	}
+	else
+		return false;
+}
+
+int Dictionary::getLength() {
+	return size;
 }
 
 void Dictionary::print() {
@@ -105,5 +131,3 @@ void Dictionary::print() {
 	}
 
 }
-
-
