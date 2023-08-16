e105e431c65cab0b286fea131deb43d68e1ae142
Cayden <S10242778@connect.np.edu.sg>
2023-08-16 16:30:51 +0800

    Menus Update B

--- DSA-ASSG2/DSA-ASSG2.cpp
+++ DSA-ASSG2/DSA-ASSG2.cpp
@@ -19,7 +19,7 @@ int LoginMenu()
 
     while (option != 0)
     {
-        cout << "---------Menu---------\n[1] Login \n[2] Register New Account[0] Exit\n----------------------" << endl;
+        cout << "-----Login Menu-------\n[1] Login \n[2] Register New Account\n[0] Exit Program\n----------------------" << endl;
         cin >> option;
 
         if (option == 1)
@@ -79,6 +79,8 @@ int LoginMenu()
         }
 
     }
+    return 0; // 0 Terminates the Program
+
 }
 
 
@@ -89,7 +91,7 @@ int customerMenu(List foodList)
 
     while (option != 0)
     {
-        cout << "---------Menu---------\n[1] Browse Food List \n[2] Create New Order\n[3] Cancel Order\n[0] Exit\n----------------------" << endl;
+        cout << "---Customer Menu---\n[1] Browse Food List \n[2] Create New Order\n[3] Cancel Order\n[0] Exit\n----------------------" << endl;
         cin >> option;
         if (option == 1)
         {
@@ -114,13 +116,13 @@ int customerMenu(List foodList)
     return -2; // logout  
 }
 
-void staffMenu()
+int staffMenu()
 {
     int option = -1;
     Order order;
     while (option != 0)
     {
-        cout << "---------Menu---------\n[1] View Upcoming Orders \n[2] Update Status of Order\n[3] View Customer Information\n[0] Exit\n----------------------" << endl;
+        cout << "------Staff Menu------\n[1] View Upcoming Orders \n[2] Update Status of Order\n[3] View Customer Information\n[0] Exit\n----------------------" << endl;
         cin >> option;
 
         if (option == 1)
@@ -145,6 +147,8 @@ void staffMenu()
         }
 
     }
+    return -2; 
+
 
 }
 
@@ -155,6 +159,7 @@ int main()
     Queue customerQueue; //for customer 
     Order order;
     Customer customer;
+    int menu = -1;
 
     List foodList;
     foodList.add("[1] WcChicken");
@@ -162,103 +167,19 @@ int main()
     foodList.add("[3] BigWac");
     foodList.add("[4] Woke Zero");
     //foodList = populateFoodList(foodList);
-
-    int staff = LoginMenu();
-    while (option != 0) {
-        cout << "---------Menu---------\n[1] View incoming orders\n[2] Update Status of an order\n[3] View Customer information for an order\n[4] Create New Order\n[5] Cancel Order\n[6] Register New Account\n[7] Log Into Account\n[0] Exit\n----------------------" << endl;
-        cout << "Select operation: ";
-        cin >> option;
-
-        if (option == 1) {
-            cout << "--View Incoming Orders--" << endl;
-            // if user is admin, allow to view incoming orders
-            orderQueue.displayItems();
-        }
-
-        else if (option == 2) {
-            cout << "--Update status of order--" << endl;
-            order.displayAllItems();
-            cout << "Current status: " << order.getStatus() << endl;
-            order.updateStatus();
-        }
-
-        else if (option == 3) {
-            cout << "--View customer information for an order--" << endl;
-            orderQueue.displayItems();
-           
-        }
-        
-        else if (option == 4) { //havent implemented adding it to orderqueue for admin to see
-            order.createNewOrder(foodList, order, orderQueue);
-        }
-
-        else if (option == 5) {
-            order.cancelOrder();
-        }
-
-        else if (option == 6)
+    
+  
+    while (menu != 0)
+    {
+        menu = LoginMenu();
+        if (menu == 1)
         {
-            string newUsername;
-            string newPassword;
-            string accountType;
-
-            cout << "Input New Username:" << endl;
-            cin >> newUsername;
-            cout << "Input New Password:" << endl;
-            cin >> newPassword; 
-
-            cout << "Input account type (Staff/Customer)" << endl;
-            cin >> accountType; 
-
-            if (accountType == "Staff")
-            {
-                staffList.add(newUsername);
-            }
-
-
-            accountDictionary.add(newPassword, newUsername);
+            menu = staffMenu();
         }
-
-        else if (option == 7)
+        else
         {
-            string username;
-            string password;
-            string retrievedUsername;
-
-            cout << "Input your username:" << endl;
-            cin >> username;
-            cout << "Input your password:" << endl;
-            cin >> password;
-
-         
-
-            retrievedUsername = accountDictionary.get(password, username);
-            if (retrievedUsername == username)
-            {
-                cout << "Successfully Logged In" << endl;
-            }
-            else
-            {
-                cout <<  retrievedUsername  << endl;
-            }
+            menu = customerMenu(foodList);
         }
-
-        else if (option == 0) {
-            cout << "Goodbye" << endl;
-            break;
-        }
-
-        else if (option == 9) {
-            accountDictionary.print();
-
-        }
-
-        else {
-            cout << "Not a valid option! Please try again" << endl;
-            continue;
-        }
-
-
     }
 }
 
